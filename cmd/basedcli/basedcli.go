055983c581d0914ce88ca0be1c15ddb4c7f49761
David Vennik <stalker.loki@protonmail.ch>
2022-03-23 11:30:23 +0200

    Added useful information and flags to cli and daemon

--- cmd/basedcli/basedcli.go
+++ cmd/basedcli/basedcli.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"os"
+)
+
+var serverAddr = flag.String("a", "localhost:50051",
+	"The server address for the basedd client to connect to",
 )
 
 func main() {
-	fmt.Println("basedcli - commandline client for based32 codec service")
+	_, _ = fmt.Fprintln(os.Stderr,
+		"basedcli - commandline client for based32 codec service",
+	)
+	flag.Parse()
 }
--- cmd/basedd/basedd.go
+++ cmd/basedd/basedd.go
@@ -1,5 +1,22 @@
 package main
 
-func main() {
+import (
+	"flag"
+	"fmt"
+	"os"
+)
 
+var serverAddr = flag.String("a", "localhost:50051",
+	"The address to listen for connections in the format of host:port "+
+		"- omit host to bind to all network interfaces",
+)
+
+func main() {
+	_, _ = fmt.Fprintln(os.Stderr,
+		"basedd - microservice for based32 human transcription encoding",
+	)
+	flag.Parse()
+	_, _ = fmt.Fprintln(os.Stderr,
+		"run with argument -h to print command line options",
+	)
 }
