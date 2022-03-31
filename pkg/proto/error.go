2e04005427bbdf98ffe375a76184020a45dcf051
David Vennik <stalker.loki@protonmail.ch>
2022-03-31 11:50:04 +0300

    Fixed error type to have an Error method.
    This means that now the generated protobuf error type can be used directly as an error return without having to define it twice.
    The generated code should already have this in it, but doesn't. It has a map to forward and reverse access it from an iota constant but doesn't integrate either the Error method to implement the error interface, nor does it use the correct type on the values in the map.

--- pkg/proto/error.go
+++ pkg/proto/error.go
@@ -1,11 +1,18 @@
 package protos
 
-import (
-	"errors"
-)
+// Error implements the Error interface which allows this error to automatically
+// generate from the error code.
+//
+// Fixes a bug in the generated code, which not
+// only lacks the Error method it uses int32 for the error string map when it
+// should be using the defined Error type. No easy way to report the bug in the
+// code.
+//
+// With this method implemented, one can simply return the error map code
+// protos.Error_ERROR_NAME_HERE and logs print this upper case snake case which
+// means it can be written to be informative in the proto file and concise in
+// usage.
+func (x Error) Error() string {
 
-// GetError compensates for a bug in protoc-gen-go not making it simpler to pull
-// the error strings from their constant values.
-func GetError(err Error) error {
-	return errors.New(Error_name[int32(err)])
+	return Error_name[int32(x)]
 }
