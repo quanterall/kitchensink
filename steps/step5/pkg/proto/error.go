de1ee7a7f2d0015d10bc172226fbf581e44c681b
David Vennik <david@cybriq.systems>
2022-05-16 09:51:07 +0300

    Removed incorrectly worded comment

--- steps/step5/pkg/proto/error.go
+++ steps/step5/pkg/proto/error.go
@@ -13,11 +13,6 @@ package proto
 // Error implements the Error interface which allows this error to automatically
 // generate from the error code.
 //
-// Fixes a bug in the generated code, which not
-// only lacks the Error method it uses int32 for the error string map when it
-// should be using the defined Error type. No easy way to report the bug in the
-// code.
-//
 // With this method implemented, one can simply return the error map code
 // protos.Error_ERROR_NAME_HERE and logs print this upper case snake case which
 // means it can be written to be informative in the proto file and concise in
